95675391616a1e4fabc90c55f0edda7f6ed0d2af
HaruTzuki <dimitris.v.91@hotmail.com>
2020-03-23 13:57:14 +0200

    Routing

--- BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -10,33 +10,12 @@ namespace BlazorMovies.Client.Pages
 {
     public partial class Counter
     {
-        [Inject] SingletonService singleton { get; set; }
-        [Inject] TrasnsientService transient { get; set; }
-        [Inject] IJSRuntime js { get; set; }
-        [CascadingParameter] public AppState AppState { get; set; }
-
+       
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
 
-        [JSInvokable]
-        public async Task IncrementCount()
+        public void IncrementCount()
         {
             currentCount++;
-            transient.Valor = currentCount;
-            singleton.Valor = currentCount;
-            currentCountStatic++;
-            await js.InvokeVoidAsync("dotnetStaticInvocation");
-        }
-
-        private async Task IncrementCountJavaScript()
-        {
-            await js.InvokeVoidAsync("dontnetInstanceInvocation", DotNetObjectReference.Create(this));
-        }
-
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
         }
     }
 }

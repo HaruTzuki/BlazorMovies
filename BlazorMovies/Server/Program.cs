4527431c2bfba633a914480f6d23485afce0597f
HaruTzuki <dimitris.v.91@hotmail.com>
2020-04-03 14:40:06 +0300

    CreatingDB

--- BlazorMovies/Server/Program.cs
+++ BlazorMovies/Server/Program.cs
@@ -9,18 +9,23 @@ using Microsoft.Extensions.Logging;
 
 namespace BlazorMovies.Server
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+			.ConfigureAppConfiguration((hostingContext, config) =>
+			{
+				config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+			})
+			.ConfigureWebHostDefaults(webBuilder =>
+			{
+					
+				webBuilder.UseStartup<Startup>();
+			});
+	}
 }
